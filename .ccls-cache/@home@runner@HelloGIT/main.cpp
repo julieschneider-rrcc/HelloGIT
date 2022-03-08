064c2ccff678127e53518a993fad1555041cdcc6
julieRRCC <91503783+julieschneider-rrcc@users.noreply.github.com>
2022-03-08 17:51:59 +0000

    Added prompt to get number

--- .ccls-cache/@home@runner@HelloGIT/main.cpp
+++ .ccls-cache/@home@runner@HelloGIT/main.cpp
@@ -3,8 +3,12 @@
 #include <iostream>
 #include <string>
 int main() {
-  std::string name;
 
+  int num = 0;
+  std::cout << "Enter Num: ";
+  std::cin >> num;
+  
+  std::string name;
   std::cout << "Name: ";
   std::cin >> name;
   std::cout << "Hello World!\t" << name << std::endl;
